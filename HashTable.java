2aff0ee27c228ee88979bc29c129a48ff27086be
willryan1 <40807997+willryan1@users.noreply.github.com>
2019-03-28 14:29:20 -0500

    Add files via upload

--- HashTable.java
+++ HashTable.java
@@ -1,4 +1,16 @@
-
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title:           P3a
+// Files:           HashTable.java,HashTableTest.java 
+//
+// Course:          CS 400, Spring Term 2019
+//
+// Author:          Will Ryan
+// Email:           wrryan@wisc.edu
+// Lecturer's Name: Debra Deppeler
+//
+///////////////////////////////////////////////////////////////////////////////
+import java.util.ArrayList;
 // TODO: comment and complete your HashTableADT implementation
 // DO ADD UNIMPLEMENTED PUBLIC METHODS FROM HashTableADT and DataStructureADT TO YOUR CLASS
 // DO IMPLEMENT THE PUBLIC CONSTRUCTORS STARTED
@@ -15,74 +27,264 @@
 //       you must use the hashCode provided by the <K key> object
 //       and one of the techniques presented in lecture
 //
+
+/**
+ * 
+ * @author Will Ryan
+ *
+ *Implements a hashtable and implements the HashTableADT interface
+ *
+ * @param <K>
+ * @param <V>
+ */
 public class HashTable<K extends Comparable<K>, V> implements HashTableADT<K, V> {
+	
+	private Object[] keys;
+	private Object[] values;
+	private HashNode<K,V>[] hashNode;
+	private int initialCapacity;
+	private double loadFactorThreshold;
+	private int size;
+	private int linearProbing;
+	
+	/**
+	 * 
+	 * @author Will Ryan
+	 *
+	 *Inner class for a HashNode
+	 *
+	 * @param <K>
+	 * @param <V>
+	 */
+	private class HashNode<K,V> {
+		K key;
+		V value;
+		private HashNode(K k, V v){
+			this.key = k;
+			this.value = v;
+		}
+	}
 	
 	// TODO: ADD and comment DATA FIELD MEMBERS needed for your implementation
 		
 	// TODO: comment and complete a default no-arg constructor
+	/**
+	 * no-arg constructor that initializes the data
+	 */
 	public HashTable() {
-
+		initialCapacity = 10;
+		keys = new Object[initialCapacity];
+		values = new Object[initialCapacity];
+		loadFactorThreshold = 0.75;
+		size = 0;
+		hashNode = new HashNode[initialCapacity];
+		linearProbing = 0;
 	}
 	
 	// TODO: comment and complete a constructor that accepts 
 	// initial capacity and load factor threshold
         // threshold is the load factor that causes a resize and rehash
 	public HashTable(int initialCapacity, double loadFactorThreshold) {
-
+		this.initialCapacity = initialCapacity;
+		this.loadFactorThreshold = loadFactorThreshold;
+		this.size = 0;
+		keys = new Object[this.initialCapacity];
+		values = new Object[this.initialCapacity];
+		hashNode = new HashNode[initialCapacity];
+		linearProbing = 0;
 	}
-
+	// TODO: add all unimplemented methods so that the class can compile
+	/**
+	 * Insert method that inserts data into the hashtable by using open 
+	 * addressing and linear probing
+	 */
 	@Override
 	public void insert(K key, V value) throws IllegalNullKeyException, DuplicateKeyException {
-		// TODO Auto-generated method stub
-		
+		if(key == null) {
+			throw new IllegalNullKeyException();
+		}if(contains(key)) {
+			throw new DuplicateKeyException();
+		}if(this.getLoadFactor() >= this.getLoadFactorThreshold()) {
+			Object[] increasedKeys = new Object[this.initialCapacity * 2 + 1];
+            Object[] increasedVals = new Object[this.initialCapacity * 2 + 1];
+            HashNode<K,V>[] increasedNode = new HashNode[this.initialCapacity * 2 +1];
+            int newCapacity = this.initialCapacity * 2 + 1;
+            int length = 0;
+            for (int i = 0; i < this.initialCapacity; i++) {
+            	length++;
+                if (this.keys[i] != null) {
+                    int hashIndex = this.getHashIndex(keys[i]);
+                    boolean same = false;
+                    for (int a = hashIndex; a < newCapacity; a++) {
+                        if (a == newCapacity - 1 && !same) {
+                            a = 0;
+                            same = true;
+                        } else if (same) {
+                            break;
+                        }
+                        if (increasedKeys[a] == null) {
+                            increasedKeys[a] = this.keys[i];
+                            increasedVals[a] = this.values[i];
+                            increasedNode[a] = this.hashNode[i];
+                            break;
+                        }
+                    }
+                }
+            }if(length > linearProbing) {
+            	linearProbing = length;
+            }
+            this.keys = increasedKeys;
+            this.values = increasedVals;
+            this.hashNode = increasedNode;
+            this.initialCapacity = this.initialCapacity * 2 + 1;
+		}
+		int hashIndex = getHashIndex(key);
+		for(int i = hashIndex; i < this.initialCapacity; i++) {
+			if(i == initialCapacity -1) {
+				i = 0;
+			}
+			if(this.keys[i] == null) {
+				this.keys[i] = key;
+				this.values[i] = value;
+				this.hashNode[i] = new HashNode(key, value);
+				this.size++;
+				break;
+			}
+		}
 	}
-
+	
+	/**
+	 * remove method that removes values by iterating through data
+	 */
 	@Override
 	public boolean remove(K key) throws IllegalNullKeyException {
-		// TODO Auto-generated method stub
-		return false;
+		if(key == null) {
+			throw new IllegalNullKeyException();
+		}if(!contains(key)) {
+			return false;
+		}
+		int hashIndex = getHashIndex(key);
+		for(int i = hashIndex; i <= hashIndex + linearProbing; i++) {
+			if(i == initialCapacity -1) {
+				i = 0;
+			}
+			if(this.keys[i] != null) {
+				if(key.compareTo(this.hashNode[i].key) == 0) {
+					this.keys[i] = null;
+					this.values[i] = null;
+					this.hashNode[i] = null; 
+					this.size--;
+					return true;
+				}
+			}
+		}return false;
 	}
-
+	
+	/**
+	 * provides the hash index
+	 * @param key
+	 * @return
+	 */
+	private int getHashIndex(Object key) {
+		int hashCode = key.hashCode();
+		int hashIndex = hashCode % this.initialCapacity;
+		return hashIndex;
+	}
+	
+	/**
+	 * gets a value from the data structure
+	 */
 	@Override
 	public V get(K key) throws IllegalNullKeyException, KeyNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		if(key == null) {
+			throw new IllegalNullKeyException();
+		}if(!contains(key)) {
+			throw new KeyNotFoundException();
+		}
+		int hashIndex = getHashIndex(key);
+		for(int i = hashIndex; i <= hashIndex + linearProbing; i++) {
+			if(i == initialCapacity - 1) {
+				i = 0;
+			}
+			if(this.keys[i] != null) {
+				if(key.compareTo(this.hashNode[i].key) == 0) {
+					return this.hashNode[i].value;
+				}
+			}
+		}return null;
 	}
-
+	
+	/**
+	 * checks if a value is in the data structure
+	 * @param key
+	 * @return
+	 */
+	private boolean contains(K key) {
+		int hashIndex = getHashIndex(key);
+		int i = hashIndex;
+		if(i<0) {
+			return false;
+		}// iterates through until the value is found
+		while(i <= initialCapacity) {
+			if(i == initialCapacity - 1) {
+				for(int j = 0; j<= hashIndex + linearProbing; j++) {
+					if(keys[i] != null) {
+						if(keys[i] == key) {
+							return true;
+						}
+					}
+				}return false;
+			}
+			if(keys[i]!= null) { // checking if a value is present
+				if(keys[i] == key) {
+					return true;
+				}
+			}i++;
+		}return false;
+	}
+	
+	/**
+	 * returns number of keys
+	 */
 	@Override
 	public int numKeys() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
-
+	
+	/**
+	 * gets the load factor threshold
+	 */
 	@Override
 	public double getLoadFactorThreshold() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.loadFactorThreshold;
 	}
-
+	
+	/**
+	 * calculates the load factor
+	 */
 	@Override
 	public double getLoadFactor() {
-		// TODO Auto-generated method stub
-		return 0;
+		if(this.initialCapacity <= 0) {
+			return 0;
+		}
+		return ((double) this.size / (double) this.initialCapacity);
 	}
-
+	
+	/**
+	 * gets the capacity
+	 */
 	@Override
 	public int getCapacity() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.initialCapacity;
 	}
-
+	
+	/**
+	 * gets the collision resolution data
+	 */
 	@Override
 	public int getCollisionResolution() {
-		// TODO Auto-generated method stub
-		return 0;
+		// 1 OPEN ADDRESSING: linear probe
+		return 1;
 	}
-
-	// TODO: add all unimplemented methods so that the class can compile
-
-
-
-
 		
 }
--- HashTableTest.java
+++ HashTableTest.java
@@ -1,4 +1,15 @@
-// TODO: add imports as needed
+//////////////////// ALL ASSIGNMENTS INCLUDE THIS SECTION /////////////////////
+//
+// Title:           P3a
+// Files:           HashTable.java,HashTableTest.java 
+//
+// Course:          CS 400, Spring Term 2019
+//
+// Author:          Will Ryan
+// Email:           wrryan@wisc.edu
+// Lecturer's Name: Debra Deppeler
+//
+///////////////////////////////////////////////////////////////////////////////
 
 import static org.junit.jupiter.api.Assertions.*; // org.junit.Assert.*; 
 import org.junit.jupiter.api.Assertions;
@@ -12,7 +23,13 @@ import java.util.Random;
 
 
 
-/** TODO: add class header comments here*/
+/**
+ * 
+ * @author Will Ryan
+ *
+ * Tests the implementation of the hashtable
+ *
+ */
 public class HashTableTest{
 
     // TODO: add other fields that will be used by multiple tests
@@ -58,6 +75,124 @@ public class HashTableTest{
             fail("insert null key should not throw exception "+e.getClass().getName());
         }
     }
+    
+    @Test
+    public void test002_test_insert() {
+    	try {
+    		HashTableADT table = new HashTable<Integer,String>();
+            table.insert("hello", "1st");
+            if(!table.get("hello").equals("1st")) {
+            	fail("insert or get worked improperly");
+            }
+    	}catch(Exception e) {
+    		fail("Unknown exception thrown");
+    	}
+    }
+    
+    @Test
+    public void test003_test_insert_remove() {
+    	try {
+    		HashTableADT table = new HashTable<Integer,String>();
+            table.insert("hello", "1st");
+            table.remove("hello");
+            table.get("hello");
+    	}catch(KeyNotFoundException e) {}
+    	catch(Exception e) {
+    		fail("Unknown exception thrown");
+    	}
+    }
+    
+    @Test
+    public void test004_test_wrong_get() {
+    	try {
+    		HashTableADT table = new HashTable<Integer,String>();
+            table.insert("hello", "1st");
+            table.get("b");
+    	}catch(KeyNotFoundException e) {}
+    	catch(Exception e) {
+    		fail("Unknown exception thrown");
+    	}
+    }
+    
+    @Test
+    public void test005_test_many_input() {
+    	try {
+    		HashTableADT table = new HashTable<Integer,String>();
+            for(int i = 0; i < 20; i++) {
+            	table.insert("insert" + i, "e" + i);
+            }
+    	}
+    	catch(Exception e) {
+    		fail("Unknown exception thrown");
+    	}
+    }
+    
+    @Test
+    public void test006_insert_many_keys_and_remove() {
+    	try {
+    		HashTableADT table = new HashTable<Integer,String>();
+            for(int i = 0; i < 50; i++) {
+            	table.insert("insert" + i, "e" + i);
+            }
+            table.remove("insert5");
+            table.remove("insert3");
+            table.remove("insert22");
+    	}
+    	catch(Exception e) {
+    		fail("Unknown exception thrown");
+    	}
+    }
+    
+    @Test
+    public void test007_insert_and_check_get_capacity() {
+    	try {
+    		HashTableADT table = new HashTable<Integer,String>(12,.75);
+            for(int i = 0; i < 3; i++) {
+            	table.insert("insert" + i, "e" + i);
+            }
+            if(table.getCapacity()!= 12) {
+            	fail("capacity did not work properly");
+            }
+    	}
+    	catch(Exception e) {
+    		fail("Unknown exception thrown");
+    	}
+    }
+    
+    @Test
+    public void test008_test_num_keys() {
+    	try {
+    		HashTableADT table = new HashTable<Integer,String>();
+            for(int i = 0; i < 50; i++) {
+            	table.insert("insert" + i, "e" + i);
+            }
+            table.remove("insert5");
+            if(table.numKeys() != 50) {
+            	fail("numKeys() did not work properly");
+            }
+    	}
+    	catch(Exception e) {
+    		fail("Unknown exception thrown");
+    	}
+    }
+    
+    @Test
+    public void test009_test_amount_of_inserts() {
+    	try {
+    		HashTableADT table = new HashTable<Integer,String>();
+            for(int i = 0; i < 100; i++) {
+            	table.insert("insert" + i, "e" + i);
+            }
+            if(table.numKeys() != 100) {
+            	fail("numKeys() did not work properly");
+            }
+    	}
+    	catch(Exception e) {
+    		fail("Unknown exception thrown");
+    	}
+    }
+    
+    
     
     // TODO add your own tests of your implementation
     
