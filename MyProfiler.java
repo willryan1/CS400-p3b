4d6f99f40a4ffeed1a69a752adecfb27187ff7ef
willryan1 <40807997+willryan1@users.noreply.github.com>
2019-03-28 13:37:53 -0500

    Add files via upload

---
+++ MyProfiler.java
@@ -0,0 +1,61 @@
+/**
+ * Filename:   MyProfiler.java
+ * Project:    p3b-201901     
+ * Authors:    TODO: add your name(s) and lecture numbers here
+ *
+ * Semester:   Spring 2019
+ * Course:     CS400
+ * 
+ * Due Date:   TODO: add assignment due date and time
+ * Version:    1.0
+ * 
+ * Credits:    TODO: name individuals and sources outside of course staff
+ * 
+ * Bugs:       TODO: add any known bugs, or unsolved problems here
+ */
+
+// Used as the data structure to test our hash table against
+import java.util.TreeMap;
+
+public class MyProfiler<K extends Comparable<K>, V> {
+
+    HashTableADT<K, V> hashtable;
+    TreeMap<K, V> treemap;
+    
+    public MyProfiler() {
+        // TODO: complete the Profile constructor
+        // Instantiate your HashTable and Java's TreeMap
+    }
+    
+    public void insert(K key, V value) {
+        // TODO: complete insert method
+        // Insert K, V into both data structures
+    }
+    
+    public void retrieve(K key) {
+        // TODO: complete the retrieve method
+        // get value V for key K from data structures
+    }
+    
+    public static void main(String[] args) {
+        try {
+            int numElements = Integer.parseInt(args[0]);
+
+            
+            // TODO: complete the main method. 
+            // Create a profile object. 
+            // For example, Profile<Integer, Integer> profile = new Profile<Integer, Integer>();
+            // execute the insert method of profile as many times as numElements
+            // execute the retrieve method of profile as many times as numElements
+            // See, ProfileSample.java for example.
+            
+        
+            String msg = String.format("Inserted and retreived %d (key,value) pairs", numElements);
+            System.out.println(msg);
+        }
+        catch (Exception e) {
+            System.out.println("Usage: java MyProfiler <number_of_elements>");
+            System.exit(1);
+        }
+    }
+}
